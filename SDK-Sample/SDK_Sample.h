b48bc5474625f8d586f34cc9a756c7398820f2f6
Zhang LiBin <zlb4077@closeli.cn>
2021-08-18 16:29:35 +0800

    Initial Commit

---
+++ SDK-Sample/SDK_Sample.h
@@ -0,0 +1,18 @@
+//
+//  SDK_Sample.h
+//  SDK-Sample
+//
+//  Created by 章立彬 on 2021/8/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDK_Sample.
+FOUNDATION_EXPORT double SDK_SampleVersionNumber;
+
+//! Project version string for SDK_Sample.
+FOUNDATION_EXPORT const unsigned char SDK_SampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDK_Sample/PublicHeader.h>
+
+
