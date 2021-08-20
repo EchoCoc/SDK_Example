b2395565a01501e31f182f06f262e474054af60a
Zhang LiBin <zlb4077@closeli.cn>
2021-08-20 15:25:58 +0800

    添加了bundle资源获取

--- SDK-Sample/Template/GetSomething.h
+++ SDK-Sample/Template/GetSomething.h
@@ -6,12 +6,15 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface GetSomething : NSObject
 
 + (void)getSomething;
+
++ (UIImage *)getBundleImage;
 
 @end
 
--- SDK-Sample/Template/GetSomething.m
+++ SDK-Sample/Template/GetSomething.m
@@ -15,4 +15,16 @@
     
 }
 
++ (UIImage *)getBundleImage {
+    
+    // 获取bundle
+    NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"Resource" ofType:@"bundle"];
+    NSBundle *resourceBundle = [NSBundle bundleWithPath:bundlePath];
+    
+    //获取图片的两种方式
+//    return [UIImage imageNamed:@"Resource.bundle/ic_anchor_unfollow"];
+    return [UIImage imageWithContentsOfFile:[resourceBundle pathForResource:@"ic_anchor_unfollow" ofType:@"png"]];
+    
+}
+
 @end
